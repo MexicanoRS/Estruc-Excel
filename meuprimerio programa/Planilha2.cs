e8230cbff4e3d5fda4b35592d9c9f9a60baea9d4
Guilherme Cervantes <guilhermecervantes@outlook.com>
2017-11-05 21:51:47 -0200

    Adicionar arquivos de projeto.

---
+++ meuprimerio programa/Planilha2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+
+namespace meuprimerio_programa
+    {
+    public partial class Planilha2
+        {
+        private void Planilha2_Startup(object sender, System.EventArgs e)
+            {
+            }
+
+        private void Planilha2_Shutdown(object sender, System.EventArgs e)
+            {
+            }
+
+        #region Código gerado pelo Designer VSTO
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InternalStartup()
+            {
+            this.Startup += new System.EventHandler(Planilha2_Startup);
+            this.Shutdown += new System.EventHandler(Planilha2_Shutdown);
+            }
+
+        #endregion
+
+        }
+    }
---
+++ meuprimerio programa/Ribbon1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+
+namespace meuprimerio_programa
+    {
+    public partial class Ribbon1
+        {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+            {
+
+            }
+
+        private void OlaMundoBtn_Click(object sender, RibbonControlEventArgs e)
+            {
+
+            Excel.Worksheet Nós = (Excel.Worksheet)Globals.ThisWorkbook.Worksheets.Add();
+            Nós.Name = "Nós";
+            Nós.get_Range("A1").Value = "Preencha a tabela abaixo de conectividade:";
+            Nós.Cells[ 2, 1 ].Value = "Nós";
+            Nós.Cells[ 2, 2 ].Value = "X";
+            Nós.Cells[ 2, 3 ].Value = "Y";
+            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+            int pos = 0;
+            for ( int i = 3; i < 203; i++ )
+                {
+                if ( pos <= 25 )
+                    {
+                    Nós.Cells[ i, 1 ].Value = String.Concat(alfabeto[ pos ]);
+                    }
+                else
+                    {
+                    Nós.Cells[ i, 1 ].Value = String.Concat(alfabeto[ (pos / 26 )-1], alfabeto[ pos - 26 * ( pos / 26 ) ]);
+                    }
+                pos++;
+
+                }
+            Excel.Range Tabela_de_Nós = Nós.Range[Nós.Cells[ 3, 1 ], Nós.Cells[ 99, 3 ]];
+            Tabela_de_Nós.Style.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            Nós.Cells[ 1, 1 ].HorizontalAlignment = Excel.XlHAlign.xlHAlignLeft;
+            }
+        }
+    }
